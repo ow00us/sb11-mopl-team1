7727bf56028d633ef2661348fd58db2a96267f3c
Yunsun <justina7182@gmail.com>
2026-07-27 18:50:23 +0900

    Fix: 코드리뷰 반영 - @Transactional 추가

--- src/main/java/com/mopl/watchingsession/repository/WatchingSessionSnapshotRepository.java
+++ src/main/java/com/mopl/watchingsession/repository/WatchingSessionSnapshotRepository.java
@@ -4,12 +4,14 @@ import com.mopl.watchingsession.entity.WatchingSessionSnapshot;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface WatchingSessionSnapshotRepository extends JpaRepository<WatchingSessionSnapshot, UUID> {
 
     // 사용자당 활성 세션 1개 -> 단건 조회, 삭제만 필요
     Optional<WatchingSessionSnapshot> findByWatcherId(UUID watcherId);
 
+    @Transactional
     void deleteByWatcherId(UUID watcherId);
 
 }
