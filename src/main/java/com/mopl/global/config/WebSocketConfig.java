4141dab734112ad08a143a4e7f5a35405f3dd70a
Yunsun <justina7182@gmail.com>
2026-08-01 10:44:52 +0900

    Feat: 에러 핸들러 추가, 하트비트 설정

--- src/main/java/com/mopl/global/config/WebSocketConfig.java
+++ src/main/java/com/mopl/global/config/WebSocketConfig.java
@@ -1,10 +1,14 @@
 package com.mopl.global.config;
 
 import com.mopl.global.security.websocket.StompAuthChannelInterceptor;
+import com.mopl.global.security.websocket.WebSocketStompErrorHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -15,22 +19,37 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final StompAuthChannelInterceptor stompAuthChannelInterceptor;
+    private final WebSocketStompErrorHandler webSocketStompErrorHandler;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-            .setAllowedOriginPatterns("*"); //TODO: 배포 환경에서는 프론트 실제 도메인으로 제한
+            .setAllowedOriginPatterns("*")  //TODO: 배포 환경에서는 프론트 실제 도메인으로 제한
+            .withSockJS();
+        registry.setErrorHandler(webSocketStompErrorHandler);
+
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/pub"); // 클라이언트 -> 서버 (SEND)
-        registry.enableSimpleBroker("/sub"); // 서버 -> 클라이언트 (SUBSCRIBE)
+        registry.enableSimpleBroker("/sub") // 서버 -> 클라이언트 (SUBSCRIBE)
+            .setHeartbeatValue(new long[]{4000, 4000})
+            .setTaskScheduler(heartbeatTaskScheduler());
     }
 
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
         registration.interceptors(stompAuthChannelInterceptor);
+    }
+
+    @Bean
+    public TaskScheduler heartbeatTaskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        scheduler.setThreadNamePrefix("ws-heartbeat-");
+        scheduler.initialize();
+        return scheduler;
     }
 
 }
