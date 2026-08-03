d89a64b61d05928d8550759194d7d6050ba43c86
LaeHee99 <148780061+LaeHee99@users.noreply.github.com>
2026-08-03 10:11:54 +0900

    feat: 유저 프로필 조회 API 구현 (#80)
    * feat: 자신의 프로필 조회 서비스 구현
    
    * feat: 자신의 프로필 조회 API 구현
    
    * fix: 사용자 조회 API를 Swagger 계약에 맞춰 수정
    
    * test: 프로필 응답의 비밀번호 필드 미노출 검증

--- src/main/java/com/mopl/user/controller/UserController.java
+++ src/main/java/com/mopl/user/controller/UserController.java
@@ -3,14 +3,19 @@ package com.mopl.user.controller;
 import com.mopl.user.dto.UserCreateRequest;
 import com.mopl.user.dto.UserDto;
 import com.mopl.user.service.UserService;
+import java.util.UUID;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+// /api/users/me 로 변환할 때 살릴 임포트
+// import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 /**
  * 사용자 회원가입 HTTP API를 처리하는 Controller
@@ -41,4 +46,47 @@ public class UserController {
             .status(HttpStatus.CREATED)
             .body(response);
     }
+
+    /**
+     * 사용자 UUID로 사용자 상세 정보를 조회
+     *
+     * Swagger에 정의된 GET /api/users/{userId} 계약
+     * URL 경로에 포함된 userId를 사용해 사용자 정보를 조회
+     *
+     * @param userId 조회할 사용자 UUID
+     * @return 사용자 상세 정보와 200 OK
+     */
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDto> findUser(
+        @PathVariable UUID userId
+    ) {
+        UserDto response = userService.findUser(userId);
+
+        return ResponseEntity.ok(response);
+    }
+
+    /* 추후 선택기능 개발 과정에서 살릴 부분 /api/users/me
+    /**
+     * 현재 인증된 사용자의 프로필을 조회
+     *
+     * 클라이언트가 사용자 ID를 path나 request body로 전달하지 않음.
+     * JwtAuthenticationFilter가 유효한 액세스 토큰에서 복원한 사용자 UUID를
+     * Spring Security의 Authentication principal에서 가져옴
+     *
+     * 이를 통해 다른 사용자의 UUID를 요청값으로 전달해
+     * 자신의 프로필인 것처럼 조회하는 문제를 방지
+     *
+     * @param userId JWT subject에서 복원된 현재 인증 사용자의 UUID
+     * @return 현재 사용자의 프로필 정보와 200 OK
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> getMyProfile(
+        @AuthenticationPrincipal UUID userId
+    ) {
+        UserDto response = userService.getMyProfile(userId);
+
+        return ResponseEntity.ok(response);
+    }
+    */
+
 }
