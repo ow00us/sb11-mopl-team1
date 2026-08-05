bf86149f062f54dc415b8974d83270c3720b2cda
jeongjae5310 <jeongjae5310@gmail.com>
2026-08-05 14:04:37 +0900

    fix: TMDB·Sports DB 설정 클래스의 Context 로딩 실패 수정 [#104]

--- src/main/java/com/mopl/content/external/tmdb/TmdbProperties.java
+++ src/main/java/com/mopl/content/external/tmdb/TmdbProperties.java
@@ -1,9 +1,7 @@
 package com.mopl.content.external.tmdb;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
-@Component
 @ConfigurationProperties(prefix = "tmdb")
 public record TmdbProperties(String baseUrl, String accessToken, String imageBaseUrl) {
 }
