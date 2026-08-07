0218fecc0a1c899b5aaef62468dac13534e522e8
jeongjae5310 <jeongjae5310@gmail.com>
2026-08-07 14:13:47 +0900

    fix: SportsDbEventItemReader 생성자를 @RequiredArgsConstructor로 통일 [#125]

--- src/main/java/com/mopl/content/external/batch/SportsDbEventItemReader.java
+++ src/main/java/com/mopl/content/external/batch/SportsDbEventItemReader.java
@@ -6,12 +6,14 @@ import java.time.LocalDate;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.batch.item.ItemReader;
 
 /**
  * (날짜 × 리그) 조합을 순회하며 Sports DB 이벤트를 읽는다. 날짜를 바깥 루프,
  * 리그를 안쪽 루프로 순회한다. Job 실행마다 상태가 초기화되어야 하므로 @StepScope 빈으로 등록한다.
  */
+@RequiredArgsConstructor
 public class SportsDbEventItemReader implements ItemReader<SportsDbEventSummary> {
 
     private final SportsDbApiClient sportsDbApiClient;
@@ -21,12 +23,6 @@ public class SportsDbEventItemReader implements ItemReader<SportsDbEventSummary>
     private int dateIndex = 0;
     private int leagueIndex = 0;
     private Iterator<SportsDbEventSummary> currentIterator = Collections.emptyIterator();
-
-    public SportsDbEventItemReader(SportsDbApiClient sportsDbApiClient, List<Integer> leagueIds, List<LocalDate> dates) {
-        this.sportsDbApiClient = sportsDbApiClient;
-        this.leagueIds = leagueIds;
-        this.dates = dates;
-    }
 
     @Override
     public SportsDbEventSummary read() {
