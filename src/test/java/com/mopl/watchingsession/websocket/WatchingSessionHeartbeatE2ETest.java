c1ebc253620694663e06582023378146accb4f0d
Yunsun <justina7182@gmail.com>
2026-08-13 13:56:47 +0900

    fix: stomp 세션 정리 사용하는 테스트 유틸로 대체

--- src/test/java/com/mopl/watchingsession/websocket/WatchingSessionHeartbeatE2ETest.java
+++ src/test/java/com/mopl/watchingsession/websocket/WatchingSessionHeartbeatE2ETest.java
@@ -11,6 +11,7 @@ import com.mopl.content.entity.Content;
 import com.mopl.content.entity.ContentType;
 import com.mopl.content.repository.ContentRepository;
 import com.mopl.global.security.JwtProvider;
+import com.mopl.support.websocket.StompTestCleanup;
 import com.mopl.user.entity.User;
 import com.mopl.user.entity.UserRole;
 import com.mopl.user.repository.UserRepository;
@@ -125,19 +126,7 @@ class WatchingSessionHeartbeatE2ETest {
 
     @AfterEach
     void tearDown() {
-        if (session != null && session.isConnected()) {
-            try {
-                session.disconnect();
-            } catch (Exception ignored) {
-                // ERROR 프레임 처리 직후 서버가 먼저 연결을 닫을 수 있습니다.
-            }
-        }
-        if (stompClient != null) {
-            stompClient.stop();
-        }
-        if (taskScheduler != null) {
-            taskScheduler.shutdown();
-        }
+        StompTestCleanup.closeAll(stompClient, taskScheduler, session);
         snapshotRepository.deleteAll();
         contentRepository.deleteAll();
         userRepository.deleteAll();
