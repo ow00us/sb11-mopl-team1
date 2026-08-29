8d3a9ee470dc2501d98aae9d2603c56e0a6b048a
shyunii <evkm37@gmail.com>
2026-08-29 12:23:45 +0900

    fix: DM 전송 요청과 저장 응답의 상관관계 보장 (#406)
    * Fix: DM 전송 요청과 저장 응답 상관관계 추가 [#400]
    
    * Test: DM 전송 요청과 저장 응답 상관관계 검증 [#400]
    
    ---------
    
    Co-authored-by: ow00us <63954536+ow00us@users.noreply.github.com>

--- src/main/java/com/mopl/directmessage/dto/DirectMessageSendRequest.java
+++ src/main/java/com/mopl/directmessage/dto/DirectMessageSendRequest.java
@@ -2,8 +2,20 @@ package com.mopl.directmessage.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
+import java.util.UUID;
+
 public record DirectMessageSendRequest(
+    UUID clientMessageId,
+
     @NotBlank(message = "메시지 내용이 없습니다.")
     String content
 ) {
+   public DirectMessageSendRequest(
+       String content
+   ) {
+       this (
+           null,
+           content
+       );
+   }
 }
